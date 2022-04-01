9902c7e73933d7197bd23c131035ea299fcd94b1
gabriela.cattin <gabriela.cattin@totvs.com.br>
2022-03-31 21:22:02 -0300

    agora foi

--- Cesta/js/script.js
+++ Cesta/js/script.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-sessionStorage.nome = document.getElementById('produto-nome').innerHTML;
->>>>>>> 3b036b6826706087477246d0486f797e3beab6a6
 sessionStorage.descricao = document.getElementById('produto-descricao').innerHTML;
 sessionStorage.preco = document.getElementById('produto-preco').innerHTML;
 img = document.getElementById('imgcompra').innerHTML;
