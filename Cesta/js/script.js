14e09f15438b8b79f4fa6d88802f24825f01b84e
gabriela.cattin <gabriela.cattin@totvs.com.br>
2022-03-31 21:04:58 -0300

    finalizado

--- Cesta/js/script.js
+++ Cesta/js/script.js
@@ -1,11 +1,9 @@
-const c = (el) => document.querySelector(el);
-const cs = (el) => document.querySelectorAll(el);
+sessionStorage.descricao = document.getElementById('produto-descricao').innerHTML;
+sessionStorage.preco = document.getElementById('produto-preco').innerHTML;
+img = document.getElementById('imgcompra').innerHTML;
 
-modelsJson.map((item, index)=>{
-    let modelsItem = c('.models .models-item').cloneNode(true);
-    modelsItem.querySelector('.models-item--img img').src = item.img; 
-    modelsItem.querySelector('.models-item--price').innerHTML = `R$ ${item.price[0].toFixed(2)}`;
-    modelsItem.querySelector('.models-item--name').innerHTML = item.name;
-    modelsItem.querySelector('.models-item--desc').innerHTML = item.description;
-    c('.models-area').append(modelsItem);
-});
+
+function botao(){
+    window.location = "../Cesta/cesta.html";
+}
+
